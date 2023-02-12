41f2e3072d31349fb35de78cf1684a2de70b0be2
Ondrej Zacha <ondrej.zacha@okra.ai>
2023-02-12 23:22:19 +0100

    Generate playlist list on refresh

--- src/volco/constants.py
+++ src/volco/constants.py
@@ -8,7 +8,10 @@ VOLUMIO_API_URL = f"{VOLUMIO_URL}:3000"
 TEMPLATE_DIR = Path("templates")
 PLAYLIST_HTML_DIR = Path("static/playlists/")  # TODO: auto mkdir
 PLAYLIST_PATTERN_PATH = Path("static/playlist_patterns.json")
+INDEX_PATH = Path("static/index.html")
 PLAYLIST_TEMPLATE_HTML = "list.html"
+INDEX_TEMPLATE_HTML = "index.html"
+
 LATEST_50_NAME = "- latest 50"
 N_LATEST = 50
 
